46bee027673e97d47609ce5ea1eb5d66b9800e08
Riley0122 <77288737+riley0122@users.noreply.github.com>
2024-07-06 22:13:57 +0200

    Add primary switch

--- project_mangaer/Program.cs
+++ project_mangaer/Program.cs
@@ -10,6 +10,23 @@ class Program
 
         if (args.Length < 1) return 1;
 
+        string action = args[0];
+
+        switch (action) {
+            case "new":
+                // Make a new project
+            break;
+            case "tag":
+                // Check if --add or --remove is set then check --name for the value
+            break;
+            case "open":
+                // Open it with the set editor
+            break;
+            default:
+                Console.WriteLine("{0} Is not a valid action!", action);
+                return 1;
+        }
+
         return 0;
     }
 }
